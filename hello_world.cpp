ddce129d72e05d90dd41b5c97a67a346e064be92
N312k <ntashirov18@gmail.com>
2025-12-03 00:06:01 +0300

    Apply clang-format with Mozilla code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
+<<<<<<<  HEAD
+	string name; // Переменная для хранения имени
+	cin >> name; // Запрос ввода
+	cout << "hello, world" << name << "!" << endl; // Вывод приветствия
+}
+====
 	string name;
 	cin >> name;
 	cout << "hello, world" << name << "!" << endl;
 }
+>>>>>>> d4e641d (Apply clang-format with Mozilla code style )
